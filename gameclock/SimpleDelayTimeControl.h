0c74c3db1216f9aa41451aa58a3d0862dc428657
Matias G. Rodriguez <matias.g.rodriguez@gmail.com>
2011-08-20 14:44:26 -0300

    Simple Delay time control cannot be implemented yet

--- gameclock/SimpleDelayTimeControl.h
+++ gameclock/SimpleDelayTimeControl.h
@@ -17,6 +17,13 @@ public:
   virtual ~SimpleDelayTimeControl() {
   }
   
+  /*
+   This time control is not supported by the game clock and TimeTracker. This time control works lie this:
+    - At the beginning of the play the clock should wait for the delay period.
+    - If the user makes a play before the delay the players keeps his original time.
+    - I the player makes a play after the delay, he retains the consumed time after the delay.
+  */
+  
   
 private:
   
