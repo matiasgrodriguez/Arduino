8c20698cb3c9f83c37ac7a66a6ff2a922c73b379
Matias G. Rodriguez <matias.g.rodriguez@gmail.com>
2011-09-14 00:06:39 -0300

    Bounce problem spike

--- gameclock/PushButton.h
+++ gameclock/PushButton.h
@@ -10,22 +10,33 @@ class PushButton {
   
   uint8_t pin;
   bool wasDown;
+  uint32_t lastUpTime;
 
 public:
 
   PushButton(uint8_t pin) {
     this->pin = pin;
     wasDown = false;
+    lastUpTime = 0;
   }
   
   void init() {
     pinMode( pin, INPUT );
+    digitalWrite( pin, HIGH ); // http://sheepdogguides.com/arduino/aht0bounce1.htm
   }
   
   void tick(Clock *clock) {
-    if( wasDown && isUp() ) {
-      wasDown = false;
+    uint32_t currentTime = clock->currentTime();
+    if( wasDown ) {
+      if( isUp() && upElapsedTime( currentTime ) > 500L  ) {
+        wasDown = false;
+        lastUpTime = currentTime;
+      }
     }
+    
+    /*if( wasDown && isUp() ) {
+      wasDown = false;
+    }*/
   }
   
   bool isUp() {
@@ -47,7 +58,11 @@ public:
   }
   
 private:
-  
+
+  uint32_t upElapsedTime(uint32_t currentTime) {
+    return currentTime - lastUpTime;
+  }
+
 };
 
 #endif
