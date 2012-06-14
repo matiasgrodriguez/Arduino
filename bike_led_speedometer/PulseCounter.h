99adfbf3056c485cadd459bf6a512e17ef976bf1
Matias G. Rodriguez <matias.g.rodriguez@gmail.com>
2012-06-14 18:22:49 -0300

    -= Bike speedometer =-

--- bike_led_speedometer/PulseCounter.h
+++ bike_led_speedometer/PulseCounter.h
@@ -8,16 +8,31 @@
 class PulseCounter {
   
   DigitalPin *pin;
+  uint32_t count;
   bool state;
 
 public:
 
   PulseCounter(DigitalPin *pin) {
     this->pin = pin;
+    count = 0;
     state = false;
   }
 
   virtual void tick(Clock *clock) {
+    bool pinState = pin->isSet();
+    if( state == pinState ) {
+      return; //no state change
+    }
+    //state changed
+    state = pinState;
+    if( state ) {
+      count++;
+    }
+  }
+  
+  uint32_t pulsesPerSecond() {
+    return count;
   }
 
 };
--- bike_led_speedometer/bike_led_speedometer.ino
+++ bike_led_speedometer/bike_led_speedometer.ino
@@ -1,4 +1,6 @@
 
+#include <arduino.h>
+
 #include "ArduinoDigitalPin.h"
 #include "ArduinoClock.h"
 #include "PulseCounter.h"
@@ -8,13 +10,17 @@ Clock *clock;
 PulseCounter *pulseCounter;
 
 void setup() {
+  Serial.begin(9600);
+    
   pulsePin = new ArduinoDigitalPin( 5, INPUT );
   clock = new ArduinoClock();
-  pulseCounter = new PulseCounter( pulsePin );
+  pulseCounter = new PulseCounter( pulsePin );  
 }
 
 void loop() {
   pulseCounter->tick( clock );
+  Serial.print( "Time: " );Serial.println( pulseCounter->pulsesPerSecond() );
+  
 }
 
 
