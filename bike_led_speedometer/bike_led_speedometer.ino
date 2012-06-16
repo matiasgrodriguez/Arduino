b3e1416e7fb8632182a6e307bbab32662675f2a0
Matias G. Rodriguez <matias.g.rodriguez@gmail.com>
2012-06-16 17:31:55 -0300

    BikeLedSpeedometer. Visual feedback with leds spike

--- bike_led_speedometer/bike_led_speedometer.ino
+++ bike_led_speedometer/bike_led_speedometer.ino
@@ -5,17 +5,28 @@
 #include "Accelerometer.h"
 
 DigitalPin *pulsePin;
+DigitalWritablePin *acceleratingPin, *deceleratingPin, *nonePin;
 Clock *clock;
 PulsesPerSecondCounter *pulseCounter;
 Accelerometer *accelerometer;
 
+void updatePins(bool,bool,bool);
+
 void setup() {
   Serial.begin(9600);
   
-  pulsePin = new ArduinoDigitalPin( 5, INPUT );
   clock = new ArduinoClock();
+  pulsePin = new ArduinoDigitalPin( 5, INPUT );
+  acceleratingPin = new ArduinoDigitalPin( 4, OUTPUT );
+  deceleratingPin = new ArduinoDigitalPin( 3, OUTPUT );
+  nonePin = new ArduinoDigitalPin( 2, OUTPUT );
+  
   pulseCounter = new PulsesPerSecondCounter( pulsePin );
   accelerometer = new Accelerometer( pulseCounter );
+  
+  acceleratingPin->set( false );
+  deceleratingPin->set( false );
+  nonePin->set( false );
 }
 
 void loop() {
@@ -27,12 +38,21 @@ void loop() {
   Serial.print( "Status: " );
   if( status == Accelerometer::None ) {
       Serial.println( "None" );
+      updatePins( true, false, false );
   } else if( status == Accelerometer::Accelerating ) {
       Serial.println( "Accelerating" );
+      updatePins( false, true, false );
   } else if ( status == Accelerometer::Decelerating ) {
     Serial.println( "Decelerating" );
+    updatePins( false, false, true );
   }
   
+}
+
+void updatePins(bool none, bool accelerating, bool decelerating) {
+  nonePin->set( none );
+  acceleratingPin->set( accelerating );
+  deceleratingPin->set( decelerating );
 }
 
 
