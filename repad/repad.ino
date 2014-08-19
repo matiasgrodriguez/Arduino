fc6a2241d8c26cc7a6128440c9795401071e574c
Matias G. Rodriguez <matias.g.rodriguez@gmail.com>
2014-08-19 13:48:19 -0300

    [Repad] Serial data upload now working! :)
     * On windows: copy mapping-numberpad.txt \\.\COM21

--- repad/repad.ino
+++ repad/repad.ino
@@ -67,14 +67,16 @@ void releasekey(const char key) {
 #define RELEASE 18
 #define RELEASEALL 19
 
-#define K_LCTRL KEY_LEFT_CTRL
-#define K_LALT KEY_LEFT_ALT
-#define K_LSHIFT KEY_LEFT_SHIFT
-#define K_PAGEUP KEY_PAGE_UP 
-#define K_PAGEDOWN KEY_PAGE_DOWN
-#define K_ENTER KEY_RETURN
-#define K_UP KEY_UP_ARROW
-#define K_DOWN KEY_DOWN_ARROW
+#define LAYOUT_COMMANDS    9
+#define LAYOUTS_COMMANDS   LAYOUT_COMMANDS*10
+
+struct Layouts {
+  int current;
+  int index;
+  byte indices[LAYOUTS_COMMANDS];
+};
+
+Layouts layouts;
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 
@@ -87,55 +89,135 @@ struct SerialUploadState {
 
 SerialUploadState serialUploadState;
 
+struct dump {
+  int size;
+  String buffer[ 30 ];
+};
+
+dump d;
+
 void serialLoop() {
   if( Serial.available() == 0 ){
     return;
   }
   
   String line = Serial.readStringUntil('\n');
+  /*
+  if( line.startsWith( "dump" ) ) {
+    Serial.print( "Dumping last " );Serial.print( d.size );Serial.println( " commands..." );
+    for(int i = 0; i < d.size; ++i) {
+      Serial.println( d.buffer[i] );
+    }
+    Serial.println( "" );
+    Serial.print( "layouts " );Serial.print( layouts.current );Serial.print( " index " );Serial.println( layouts.index );
+    for(int i=0; i < layouts.index; ++i) {
+      Serial.println( layouts.indices[ i ] );
+    }
+
+    return;
+  }
+  d.buffer[ d.size++ ] = line;
+  */
+  
+  boolean needsToUpdateLayout = false;
   byte command = 0;
-  if( line.equals( "bs" ) ) {
-    Serial.println( "BUFFER_START" );
+  
+  if( line.startsWith( "bs" ) ) {
     serialUploadState.current = 0;
     command = BUFFER_START;
-  } else if( line.equals( "ks" ) ) {
-    Serial.println( "KEY_START" );
+  } else if( line.startsWith( "ks" ) ) {
     command = KEY_START;
-  } else if( line.equals( "wa" ) ) {
-    Serial.println( "WAIT" );
+  } else if( line.startsWith( "wa" ) ) {
     command = WAIT;
-  } else if( line.equals( "rk" ) ) {
-    Serial.println( "RELEASE" );
+  } else if( line.startsWith( "rk" ) ) {
     command = RELEASE;
-  } else if( line.equals( "ra" ) ) {
-    Serial.println( "RELEASEALL" );
+  } else if( line.startsWith( "ralt" ) ) {
+    command = KEY_RIGHT_ALT;
+  } else if( line.startsWith( "ra" ) ) {
     command = RELEASEALL;
-  } else if( line.equals( "be" ) ) {
-    Serial.println( "BUFFER_END" );
-    command = BUFFER_END;
-  } /*else if( line.equals( "" ) ) {
-    command = ;
-  } else if( line.equals( "" ) ) {
-    command = ;
-  } else if( line.equals( "" ) ) {
-    command = ;
-  } else if( line.equals( "" ) ) {
-  }*/
-  //EEPROM.write( serialUploadState.current++, command );
+  } else if( line.startsWith( "be" ) ) {
+    command = BUFFER_END; 
+    needsToUpdateLayout = true;
+  } else if( line.startsWith( "lctrl" ) ) {
+    command = KEY_LEFT_CTRL;
+  } else if( line.startsWith( "lshift" ) ) {
+    command = KEY_LEFT_SHIFT;
+  } else if( line.startsWith( "lalt" ) ) {
+    command = KEY_LEFT_ALT;
+  } else if( line.startsWith( "lgui" ) ) {
+    command = KEY_LEFT_GUI;
+  } else if( line.startsWith( "rctrl" ) ) {
+    command = KEY_RIGHT_CTRL;
+  } else if( line.startsWith( "rshift" ) ) {
+    command = KEY_RIGHT_SHIFT;
+  } else if( line.startsWith( "rgui" ) ) {
+    command = KEY_RIGHT_GUI;
+  } else if( line.startsWith( "up" ) ) {
+    command = KEY_UP_ARROW;
+  } else if( line.startsWith( "down" ) ) {
+    command = KEY_DOWN_ARROW;
+  } else if( line.startsWith( "left" ) ) {
+    command = KEY_LEFT_ARROW;
+  } else if( line.startsWith( "right" ) ) {
+    command = KEY_RIGHT_ARROW;
+  } else if( line.startsWith( "backspace" ) ) {
+    command = KEY_BACKSPACE;
+  } else if( line.startsWith( "tab" ) ) {
+    command = KEY_TAB;
+  } else if( line.startsWith( "enter" ) ) {
+    command = KEY_RETURN;
+  } else if( line.startsWith( "esc" ) ) {
+    command = KEY_ESC;
+  } else if( line.startsWith( "ins" ) ) {
+    command = KEY_INSERT;
+  } else if( line.startsWith( "del" ) ) {
+    command = KEY_DELETE;
+  } else if( line.startsWith( "pup" ) ) {
+    command = KEY_PAGE_UP;
+  } else if( line.startsWith( "pdown" ) ) {
+    command = KEY_PAGE_DOWN;
+  } else if( line.startsWith( "home" ) ) {
+    command = KEY_HOME;
+  } else if( line.startsWith( "end" ) ) {
+    command = KEY_END;
+  } else if( line.startsWith( "capsl" ) ) {
+    command = KEY_CAPS_LOCK;
+  } else if( line.startsWith( "f10" ) ) {
+    command = KEY_F10;
+  } else if( line.startsWith( "f11" ) ) {
+    command = KEY_F11;
+  } else if( line.startsWith( "f12" ) ) {
+    command = KEY_F12;
+  } else if( line.startsWith( "f1" ) ) {
+    command = KEY_F1;
+  } else if( line.startsWith( "f2" ) ) {
+    command = KEY_F2;
+  } else if( line.startsWith( "f3" ) ) {
+    command = KEY_F3;
+  } else if( line.startsWith( "f4" ) ) {
+    command = KEY_F4;
+  } else if( line.startsWith( "f5" ) ) {
+    command = KEY_F5;
+  } else if( line.startsWith( "f6" ) ) {
+    command = KEY_F6;
+  } else if( line.startsWith( "f7" ) ) {
+    command = KEY_F7;
+  } else if( line.startsWith( "f8" ) ) {
+    command = KEY_F8;
+  } else if( line.startsWith( "f9" ) ) {
+    command = KEY_F9;
+  } else {
+    command = line.charAt(0);
+  }
+  //Serial.print( "cmd-> " );Serial.println( command );
+  EEPROM.write( serialUploadState.current++, command );
+  
+  if( needsToUpdateLayout ) {
+    setupCommands();
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
-
-#define LAYOUT_COMMANDS    9
-#define LAYOUTS_COMMANDS   LAYOUT_COMMANDS*10
-
-struct Layouts {
-  int current;
-  int index;
-  byte indices[LAYOUTS_COMMANDS];
-};
-
-Layouts layouts;
 
 void storeHardCodedValues() {
   const size_t MAX_SIZE = 512;
@@ -146,42 +228,42 @@ void storeHardCodedValues() {
   commands[i++] = BUFFER_START; //buffer start
 
   commands[i++] = KEY_START; //shortcut start: 0 eclipse previous
-  commands[i++] = K_LALT;
-  commands[i++] = K_LSHIFT;
-  commands[i++] = K_UP;
+  commands[i++] = KEY_LEFT_ALT;
+  commands[i++] = KEY_LEFT_SHIFT;
+  commands[i++] = KEY_UP_ARROW;
   commands[i++] = WAIT;
   commands[i++] = RELEASEALL;
 
   commands[i++] = KEY_START; //shortcut start: 1 eclipse action
-  commands[i++] = K_LCTRL;
+  commands[i++] = KEY_LEFT_CTRL;
   commands[i++] = '1';
   commands[i++] = WAIT;
   commands[i++] = RELEASEALL;
 
   commands[i++] = KEY_START; //shortcut start: 2 eclipse next
-  commands[i++] = K_LALT;
-  commands[i++] = K_LSHIFT;
-  commands[i++] = K_DOWN;
+  commands[i++] = KEY_LEFT_ALT;
+  commands[i++] = KEY_LEFT_SHIFT;
+  commands[i++] = KEY_DOWN_ARROW;
   commands[i++] = WAIT;
   commands[i++] = RELEASEALL;
 
   commands[i++] = KEY_START; //shortcut start: 3 eclipse variable
-  commands[i++] = K_LALT;
-  commands[i++] = K_LSHIFT;
+  commands[i++] = KEY_LEFT_ALT;
+  commands[i++] = KEY_LEFT_SHIFT;
   commands[i++] = 'l'; 
   commands[i++] = WAIT; 
   commands[i++] = RELEASEALL; 
   
   commands[i++] = KEY_START; //shortcut start: 4 eclipse rename
-  commands[i++] = K_LALT;
-  commands[i++] = K_LSHIFT;
+  commands[i++] = KEY_LEFT_ALT;
+  commands[i++] = KEY_LEFT_SHIFT;
   commands[i++] = 'r'; 
   commands[i++] = WAIT; 
   commands[i++] = RELEASEALL; 
   
   commands[i++] = KEY_START; //shortcut start: 5 eclipse method
-  commands[i++] = K_LALT;
-  commands[i++] = K_LSHIFT;
+  commands[i++] = KEY_LEFT_ALT;
+  commands[i++] = KEY_LEFT_SHIFT;
   commands[i++] = 'm'; 
   commands[i++] = WAIT; 
   commands[i++] = RELEASEALL; 
@@ -189,8 +271,8 @@ void storeHardCodedValues() {
   commands[i++] = KEY_START; //shortcut start: 6 eclipse ?
   
   commands[i++] = KEY_START; //shortcut start: 7 eclipse inline
-  commands[i++] = K_LALT;
-  commands[i++] = K_LSHIFT;
+  commands[i++] = KEY_LEFT_ALT;
+  commands[i++] = KEY_LEFT_SHIFT;
   commands[i++] = 'i'; 
   commands[i++] = WAIT; 
   commands[i++] = RELEASEALL; 
@@ -200,25 +282,25 @@ void storeHardCodedValues() {
   ////////////
   
   commands[i++] = KEY_START; //shortcut start: 0 resharper previous
-  commands[i++] = K_LALT;
-  commands[i++] = K_PAGEUP;
+  commands[i++] = KEY_LEFT_ALT;
+  commands[i++] = KEY_PAGE_UP;
   commands[i++] = WAIT;
   commands[i++] = RELEASEALL;
 
   commands[i++] = KEY_START; //shortcut start: 0 resharper action
-  commands[i++] = K_LALT;
-  commands[i++] = K_ENTER;
+  commands[i++] = KEY_LEFT_ALT;
+  commands[i++] = KEY_RETURN;
   commands[i++] = WAIT;
   commands[i++] = RELEASEALL;
 
   commands[i++] = KEY_START; //shortcut start: 0 resharper next
-  commands[i++] = K_LALT;
-  commands[i++] = K_PAGEDOWN;
+  commands[i++] = KEY_LEFT_ALT;
+  commands[i++] = KEY_PAGE_DOWN;
   commands[i++] = WAIT;
   commands[i++] = RELEASEALL;
 
   commands[i++] = KEY_START; //shortcut start: 0 resharper variable
-  commands[i++] = K_LCTRL;
+  commands[i++] = KEY_LEFT_CTRL;
   commands[i++] = 'r';
   commands[i++] = WAIT;
   commands[i++] = RELEASE;
@@ -229,7 +311,7 @@ void storeHardCodedValues() {
   commands[i++] = RELEASEALL;
 
   commands[i++] = KEY_START; //shortcut start: 0 resharper rename
-  commands[i++] = K_LCTRL;
+  commands[i++] = KEY_LEFT_CTRL;
   commands[i++] = 'r';
   commands[i++] = WAIT;
   commands[i++] = RELEASE;
@@ -240,7 +322,7 @@ void storeHardCodedValues() {
   commands[i++] = RELEASEALL;
 
   commands[i++] = KEY_START; //shortcut start: 0 resharper method
-  commands[i++] = K_LCTRL;
+  commands[i++] = KEY_LEFT_CTRL;
   commands[i++] = 'r';
   commands[i++] = WAIT;
   commands[i++] = RELEASE;
@@ -253,7 +335,7 @@ void storeHardCodedValues() {
   commands[i++] = KEY_START; //shortcut start: 0 resharper ?
 
   commands[i++] = KEY_START; //shortcut start: 0 resharper inline
-  commands[i++] = K_LCTRL;
+  commands[i++] = KEY_LEFT_CTRL;
   commands[i++] = 'r';
   commands[i++] = WAIT;
   commands[i++] = RELEASE;
@@ -339,6 +421,7 @@ void setup(){
   Serial.begin(9600);
   Keyboard.begin();
   
+  d.size = 0;
   setupCommands();
 }
 
